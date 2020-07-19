941166cc1c588023082fedb604e81d52d49fe2a3
Vladimir <komarov2142@gmail.com>
2020-07-19 20:52:46 +1000

    user actions update

--- src/constants/authConstants.js
+++
@@ -1,4 +0,0 @@
-export const authConstants = {
-    logIn: "LOG_IN",
-    register: "REGISTER",
-}
--- src/constants/userConstants.js
+++ src/constants/userConstants.js
@@ -13,13 +13,32 @@
 // }
 
 export const userConstants = {
-    addRole: "CHANGE_ROLE",
-    removeRole: "REMOVE_ROLE",
-    setName: "SET_NAME",
-    setSurname: "SET_SURNAME",
-    setEmail: "SET_EMAIL",
-    setPhone: "SET_PHONE",
-    setState: "SET_STATE",
-    setCity: "SET_CITY",
-    setAddress: "SET_ADDRESS"
+    registerRequest: "USER_REGISTER_REQUEST",
+    registerSuccess: "USER_REGISTER_SUCCESS",
+    registerFailure: "USER_REGISTER_FAILURE",
+
+    loginRequest: "USER_LOGIN_REQUEST",
+    loginSuccess: "USER_LOGIN_SUCCESS",
+    loginFailure: "USER_LOGIN_FAILURE",
+
+    logout: "USER_LOGOUT",
+
+    deleteRequest: "USER_LOGIN_REQUEST",
+    deleteSuccess: "USER_LOGIN_SUCCESS",
+    deleteFailure: "USER_LOGIN_FAILURE",
+
+    getAllUsersRequest: "GET_ALL_USERS_REQUEST",
+    getAllUsersSuccess: "GET_ALL_USERS_SUCCESS",
+    getAllUsersFailure: "GET_ALL_USERS_FAILURE",
+
+    addRole: "CHANGE_USER_ROLE",
+    removeRole: "REMOVE_USER_ROLE",
+
+    setName: "SET_USER_NAME",
+    setSurname: "SET_USER_SURNAME",
+    setEmail: "SET_USER_EMAIL",
+    setPhone: "SET_USER_PHONE",
+    setState: "SET_USER_STATE",
+    setCity: "SET_USER_CITY",
+    setAddress: "SET_USER_ADDRESS"
 }
